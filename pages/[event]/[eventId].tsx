58151c06c876b43e6f022af2f122b940f1e0d0a2
Sydney Essex <sydney@vibe.video>
2023-02-14 17:54:09 -0500

    fix: background mobile sizing

--- pages/[event]/[eventId].tsx
+++ pages/[event]/[eventId].tsx
@@ -217,14 +217,14 @@ const SubGallery = (props: ResponseData) => {
       </label> */}
 
       <section
-        className={`text-white bg-black min-h-screen p-10 ${!_.isEmpty(event) && 'pt-0'}`}
+        className={`text-white bg-black min-h-screen p-10 ${!_.isEmpty(event) && 'pt-0'} relative`}
         style={event.background ? { 
           background: `url(${event.background}) no-repeat center center fixed`, 
-          backgroundSize: 'cover',
+          backgroundSize: '100% 100vh',
           //@ts-ignore
-          '-webkit-background-size': 'cover',
-          '-moz-background-size': 'cover',
-          '-o-background-size': 'cover'
+          '-webkit-background-size': '100% 100vh',
+          '-moz-background-size': '100% 100vh',
+          '-o-background-size': '100% 100vh'
           } : {}}>
 
         <div className='flex justify-center'>
