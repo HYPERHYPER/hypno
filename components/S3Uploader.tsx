52628d4f12ffa46b6bdfa9009bad2768ee73bf92
Sydney Essex <sydney@vibe.video>
2023-02-21 15:24:00 -0500

    feat: configure event gallery
    commit 5d3c8f1fb54f32b178f86412a1a114ed20678ed3
    Author: Sydney Essex <sydney@vibe.video>
    Date:   Tue Feb 21 15:10:21 2023 -0500
    
        simplify enable data capture screen
    
    commit a4dc4b57e07052fd8859ba1490336a0468b1e02c
    Author: Sydney Essex <sydney@vibe.video>
    Date:   Tue Feb 21 15:02:59 2023 -0500
    
        cleanup
    
    commit abb37537fe63004750e5af9a984f43dd3e41c8b8
    Author: Sydney Essex <sydney@vibe.video>
    Date:   Tue Feb 21 14:59:59 2023 -0500
    
        feat: token access to config event
    
    commit 3902af6e4bc4957547770c9d45c05c8b0dfc4a07
    Author: Sydney Essex <sydney@vibe.video>
    Date:   Tue Feb 21 13:37:42 2023 -0500
    
        feat: config mini gallery event interface
    
    commit f75d9aee3ec6d8968409025ad1f1c468b14939a0
    Author: Sydney Essex <sydney@vibe.video>
    Date:   Wed Feb 15 15:22:59 2023 -0500
    
        wip manage event gallery

--- components/S3Uploader.tsx
+++ components/S3Uploader.tsx
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { useState, useRef, SyntheticEvent } from 'react';
+import { ThreeDots } from 'react-loader-spinner';
 
 interface UploaderProps {
   eventName: string;
+  label: string;
+  inputId?: string;
+  onInputChange?: (value: string) => void;
+  value?: string;
 }
 
 export default function S3Uploader(props: UploaderProps) {
@@ -12,9 +17,12 @@ export default function S3Uploader(props: UploaderProps) {
   const [newFilename, setNewFilename] = useState('');
   const [assetLocation, setAssetLocation] = useState('');
 
+  const [uploadStatus, setUploadStatus] = useState<'uploading' | 'completed' | 'error'>();
+
   const onFileChange = (e: SyntheticEvent) => {
     let files = (e.target as HTMLInputElement).files
     if (!files?.length) return;
+    setUploadStatus(undefined);
     createImage(files[0]);
   };
 
@@ -28,15 +36,17 @@ export default function S3Uploader(props: UploaderProps) {
       // if (e.target.result.length > MAX_IMAGE_SIZE) {
       //   return alert("Image is loo large.");
       // }
-	  if (e.target?.result) {
-      	setFile(e.target.result);
-      	setContentType((e.target.result as string).split(':')[1].split(';')[0]);
-	  }
+      if (e.target?.result) {
+        setFile(e.target.result);
+        setContentType((e.target.result as string).split(':')[1].split(';')[0]);
+      }
     };
     reader.readAsDataURL(file);
   };
 
-  const uploadToS3 = async () => {
+  const uploadToS3 = async (e: any) => {
+    e.preventDefault();
+    setUploadStatus('uploading')
     const url = process.env.NEXT_PUBLIC_AWS_ENDPOINT as string;
     const resp = await axios.get(url, {
       params: { fileName: props.eventName + '/' + newFilename, contentType },
@@ -58,49 +68,75 @@ export default function S3Uploader(props: UploaderProps) {
 
     if (result.status == 200) {
       setAssetLocation(result.url.split('?')[0]);
+      props.onInputChange && props.onInputChange(result.url.split('?')[0]);
       if (fileRef.current) {
-		fileRef.current.value = result.url.split('?')[0];
-	  } 
+        fileRef.current.value = result.url.split('?')[0];
+      }
+      setUploadStatus('completed')
+    } else {
+      setUploadStatus('error')
     }
   };
 
+  const resetInput = () => {
+    setAssetLocation('');
+    props.onInputChange && props.onInputChange('');
+    if (fileRef.current) {
+      fileRef.current.value = '';
+    }
+  }
+
   return (
     <>
-      <div className='form-control w-full max-w-xs'>
-        <label className='label text-right justify-end'>
-          <label
-            htmlFor='upload-modal'
-            className='label-text-alt cursor-pointer'
-          >
-            Upload
+      <div className='form-control w-full'>
+        <label className='label'>
+          <label className='label-text'>{props.label}</label>
+          <label className='label-text-alt text-white/30 flex flex-row gap-3'>
+            <label htmlFor={`${props.inputId}-upload-modal`} className='cursor-pointer hover:text-white transition'>Upload</label>
+            <label className='cursor-pointer hover:text-red-400 transition' onClick={resetInput}>Reset</label>
           </label>
         </label>
         <input
           type='text'
-          placeholder='asset url'
-          className='input input-bordered w-full max-w-xs'
+          placeholder='Asset URL'
+          className='input input-bordered w-full'
           ref={fileRef}
+          disabled
+          id={props.inputId}
+          value={props.value}
         />
       </div>
 
-      <input type='checkbox' id='upload-modal' className='modal-toggle' />
-      <label htmlFor='upload-modal' className='modal cursor-pointer'>
-        <label className='modal-box relative' htmlFor='upload-modal'>
+      <input type='checkbox' id={`${props.inputId}-upload-modal`} className='modal-toggle' />
+      <label htmlFor={`${props.inputId}-upload-modal`} className='modal cursor-pointer'>
+        <label className='modal-box relative text-black' htmlFor={`${props.inputId}-upload-modal`}>
           <label
-            htmlFor='upload-modal'
-            className='btn btn-sm btn-circle absolute right-2 top-2'
+            htmlFor={`${props.inputId}-upload-modal`}
+            className='btn btn-sm btn-square rounded-full absolute right-3 top-3'
           >
             ✕
           </label>
-          <h3 className='text-lg font-bold'>select media for upload</h3>
+          <h3 className='text-lg font-medium'>Select media for upload</h3>
           <input
             type='file'
-            className='file-input w-full max-w-xs mt-3'
+            className='file-input input-bordered w-full mt-3'
             onChange={onFileChange}
           />
+
           <div className='modal-action'>
-            <button className='btn' onClick={uploadToS3}>
-              upload
+            <button className={`btn btn-block rounded-full ${uploadStatus == 'completed' ? 'btn-success' : ''}`} disabled={uploadStatus == 'uploading'} onClick={uploadToS3}>
+              {uploadStatus == 'uploading' ? (
+                <ThreeDots
+                  height="20"
+                  width="50"
+                  radius="4"
+                  color="#3D4451"
+                  ariaLabel="three-dots-loading"
+                  visible={true}
+                />
+              ) : 
+                uploadStatus == 'completed' ? 'Success!' : 'Upload'
+              }
             </button>
           </div>
         </label>
---
+++ pages/manage/[eventId].tsx
@@ -0,0 +1,300 @@
+import axios from 'axios';
+import type { GetServerSideProps } from 'next';
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import _ from 'lodash';
+import { useForm } from 'react-hook-form';
+import Image from 'next/image';
+import S3Uploader from '@/components/S3Uploader';
+import { ThreeDots } from 'react-loader-spinner';
+import nookies, { parseCookies } from 'nookies'
+
+
+interface ResponseData {
+    status: number;
+    message: string;
+    event: any;
+}
+
+const DEFAULT_TERMS = `By pressing "continue" to access and save your content, you accept the Terms of Use and Privacy Policy provided by Hypno and related partners`
+
+
+const ManageEventGallery = (props: ResponseData) => {
+    const { event: {
+        id,
+        name,
+        metadata,
+        terms_and_conditions,
+    } } = props;
+
+    const [view, setView] = useState<'basic' | 'data'>('basic');
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+        setValue,
+    } = useForm({
+        defaultValues: {
+            gallery_title: metadata.gallery_title || '',
+            gallery_subtitle: metadata.gallery_subtitle || '',
+            data_capture_screen: metadata.data_capture_screen || false,
+            data_capture_title: metadata.data_capture_title || '',
+            data_capture_subtitle: metadata.data_capture_subtitle || '',
+            fields: metadata.fields || [],
+            logo: metadata.logo || '',
+            background: metadata.background || '',
+            color: metadata.color || '',
+            terms_and_conditions: terms_and_conditions || DEFAULT_TERMS,
+        }
+    });
+
+    const config = watch();
+
+    const [savedChangesStatus, setSavedChangesStatus] = useState<'saving' | 'completed'>()
+
+    useEffect(() => {
+        if (savedChangesStatus == 'completed') {
+            setTimeout(() => {
+                setSavedChangesStatus(undefined)
+            }, 3000)
+        }
+    }, [config]);
+
+    const submitForm = async (data: any) => {
+        setSavedChangesStatus('saving');
+        if (!_.isEmpty(errors)) {
+            console.log("submitForm errors", { errors });
+            return;
+        }
+
+        console.log("submitForm", { data });
+        const terms_and_conditions = data.terms_and_conditions;
+        delete data['terms_and_conditions']
+
+        /* Update metadata field of event */
+        let eventMetadata = props.event.metadata || {};
+        eventMetadata = {
+            ...props.event.metadata,
+            ...data,
+            fields: (!_.isEmpty(data.fields) && _.first(data.fields) != '') ? _.map(_.split(data.fields, ','), (f) => f.trim()) : [],
+        }
+
+        const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/events/${id}.json`;
+        const token = parseCookies().hypno_token;
+        let resp = await axios.put(url, { metadata: { ...eventMetadata }, terms_and_conditions }, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: 'Bearer ' + token,
+            },
+        }).then((e) => {
+            console.log(e)
+            setSavedChangesStatus('completed')
+        }).catch((e) => {
+            console.log(e)
+        });
+    }
+
+    return (
+        <>
+            <Head>
+                <title>{name + ' Admin' || 'Hypno Pro Admin'}</title>
+            </Head>
+
+            <section className={`text-white bg-white/10 min-h-screen relative`}>
+                <div className='sticky top-0 bg-black'>
+                    <div className='mx-auto px-[30px] py-[35px] flex sm:flex-row justify-between items-center'>
+                        <h1 className='text-2xl'>Edit Gallery <span className='ml-1 text-gray-400'>{name}</span></h1>
+                        <Image className='w-auto' src={'https://hypno-web-assets.s3.amazonaws.com/hypno-logo-white-drop.png'} alt={"Hypno logo"} width={100} height={100} priority />
+                    </div>
+                </div>
+
+                <div className={`sm:mx-auto h-full mx-7 pb-[30px] flex flex-row w-full`}>
+                    <div className='mt-8 w-1/3 p-4'>
+                        <div className="tabs gap-2 font-medium bg-transparent flex flex-col">
+                            <a className={`tab tab-lg text-white ${view == 'basic' ? 'tab-active' : ''}`} onClick={() => setView('basic')}>Basic</a>
+                            <a className={`tab tab-lg text-white ${view == 'data' ? 'tab-active' : ''}`} onClick={() => setView('data')}>Data Capture</a>
+                        </div>
+                    </div>
+                    <form onSubmit={handleSubmit(submitForm)} className='mt-8 space-y-3 flex-1 flex flex-col p-4 pb-5 max-w-lg'>
+                        {view == 'basic' && (
+                            <>
+                                <div className='form-control '>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Gallery Title</span>
+                                    </label>
+                                    <input
+                                        className='input'
+                                        placeholder='Share and tag all over social.'
+                                        {...register('gallery_title')} />
+                                </div>
+
+                                <div className='form-control '>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Gallery Subtitle</span>
+                                    </label>
+                                    <input
+                                        className='input'
+                                        placeholder='#hypno #pro #iphone'
+                                        {...register('gallery_subtitle')} />
+                                </div>
+
+                                <div>
+                                    <S3Uploader
+                                        eventName={id}
+                                        label={'Logo'}
+                                        inputId='logo'
+                                        onInputChange={(value: string) => setValue('logo', value)} />
+                                </div>
+
+                                <div>
+                                    <S3Uploader
+                                        eventName={id}
+                                        label={'Background'}
+                                        inputId='background'
+                                        onInputChange={(value: string) => setValue('background', value)}
+                                        value={config.background}
+                                    />
+                                </div>
+
+                                <div className='form-control indicator w-full'>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Button Color</span>
+                                        <span className='label-text-alt'>
+                                        </span>
+                                    </label>
+                                    <input
+                                        className='input'
+                                        placeholder='Hex Color Code'
+                                        {...register('color')} />
+                                    {config.color && <span className="indicator-item indicator-middle translate-y-[40%] -translate-x-3/4 indicator-end badge" style={{ backgroundColor: config.color }}></span>}
+                                </div>
+                            </>
+                        )}
+
+                        {view == 'data' && (
+                            <>
+                                <div className='form-control'>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Data Capture Screen</span>
+                                    </label>
+                                    <div className='flex flex-row gap-2 items-center'>
+                                        <input type="checkbox" className="toggle toggle-lg" {...register('data_capture_screen')} />
+                                        <span className='text-sm text-white/40'>{config.data_capture_screen ? 'Enabled' : 'Disabled'}</span>
+                                    </div>
+                                </div>
+
+                                <div className='form-control'>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Data Capture Fields</span>
+                                    </label>
+                                    <input
+                                        className='input'
+                                        placeholder='Name, Email'
+                                        disabled={!config.data_capture_screen}
+                                        {...register('fields')} />
+                                </div>
+
+                                <div className='form-control'>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Data Capture Title</span>
+                                    </label>
+                                    <input
+                                        className='input'
+                                        placeholder='Want your photos?'
+                                        disabled={!config.data_capture_screen}
+                                        {...register('data_capture_title')} />
+                                </div>
+
+                                <div className='form-control'>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Data Capture Subtitle</span>
+                                    </label>
+                                    <input
+                                        className='input'
+                                        placeholder='Add your info to continue'
+                                        disabled={!config.data_capture_screen}
+                                        {...register('data_capture_subtitle')} />
+                                </div>
+
+                                <div className='form-control'>
+                                    <label className='label'>
+                                        <span className='label-text text-white'>Terms and Conditions</span>
+                                        <span className='label-text-alt cursor-pointer text-white/40 hover:text-white transition' onClick={() => setValue('terms_and_conditions', DEFAULT_TERMS)}>Reset</span>
+                                    </label>
+                                    <textarea className='textarea w-full leading-[1.1rem]' rows={3} disabled={!config.data_capture_screen} {...register('terms_and_conditions')} />
+                                </div>
+                            </>
+                        )}
+                        <div className='fixed bottom-[30px] right-1/2 translate-x-1/2'>
+                            {savedChangesStatus ? (
+                                <button className='btn btn-wide rounded-full shadow-lg'>{savedChangesStatus == 'saving' ?
+                                    <ThreeDots
+                                        height="20"
+                                        width="50"
+                                        radius="4"
+                                        color="#FFFFFF"
+                                        ariaLabel="three-dots-loading"
+                                        visible={true}
+                                    />
+                                    :
+                                    'CHANGES SAVED!'
+                                }</button>
+                            ) : 
+                                <input className='btn btn-primary btn-wide rounded-full shadow-lg' type='submit' value='SAVE' />
+                            }
+                        </div>
+
+                    </form>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { eventId, accessToken : accessTokenParam } = context.query;
+    const cookieToken = nookies.get(context).hypno_token;
+    let accessToken = accessTokenParam || cookieToken;
+    if (!accessToken) {
+        return {
+            notFound: true,
+        }
+    }
+
+    // Store access token in case of refresh
+    if (cookieToken !== accessToken) {
+        nookies.set(context, 'hypno_token', String(accessToken));
+    }
+
+    // Fetch event config
+    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/events/${eventId}.json`;
+    const token = accessToken;
+    let data = {};
+    await axios.get(url, {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+        },
+    }).then(async (res) => {
+        if (res.status === 200) {
+            data = await res.data;
+        }
+    }).catch((e) => {
+        console.log(e);
+    })
+
+    if (_.isEmpty(data)) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props: {
+            ...data,
+        }
+    };
+};
+
+export default ManageEventGallery;
