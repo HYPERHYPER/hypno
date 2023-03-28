de1c4d1ffad9845b21a921737153f735b495bce6
Sydney Essex <sydney@vibe.video>
2023-03-28 16:26:04 -0400

    fix: fixed background

--- components/Gallery/CustomGallery.tsx
+++ components/Gallery/CustomGallery.tsx
@@ -21,24 +21,27 @@ type EventData = {
 
 export function CustomGallery({ event, children }: { event: EventData, children: React.ReactNode }) {
     return (
-        <section
-            className={`text-white bg-black min-h-screen px-6 pb-10`}
-            style={event.background ? {
-                background: `url(${event.background}) no-repeat center center fixed`,
-                backgroundSize: 'cover',
-                WebkitBackgroundSize: 'cover',
-                MozBackgroundSize: 'cover',
-                //@ts-ignore
-                '-o-background-size': 'cover'
-            } : {}}>
+        <section className={`text-white min-h-screen`}>
+            <div
+                className='fixed bg-black top-0 bottom-0 left-0 w-screen h-screen'
+                style={event.background ? {
+                    background: `url(${event.background}) no-repeat center center fixed`,
+                    backgroundSize: 'cover',
+                    WebkitBackgroundSize: 'cover',
+                    MozBackgroundSize: 'cover',
+                    OBackgroundSize: 'cover'
+                } : {}}
+            />
 
-            <div className='min-h-[85px] pt-[30px]'>
-                <div className='flex justify-center'>
-                    <Image className='h-[25px] w-auto' src={event.logo ? event.logo : 'https://hypno-web-assets.s3.amazonaws.com/hypno-logo-white-drop.png'} alt={event.name + " logo"} width={150} height={25} priority />
+            <div className='fixed top-0 bottom-0 left-0 right-0 overflow-x-hidden overflow-y-scroll px-6'>
+                <div className='min-h-[85px] pt-[30px] w-full'>
+                    <div className='flex justify-center'>
+                        <Image className='h-[25px] w-auto sm:h-[30px]' src={event.logo ? event.logo : 'https://hypno-web-assets.s3.amazonaws.com/hypno-logo-white-drop.png'} alt={event.name + " logo"} width={150} height={25} priority />
+                    </div>
                 </div>
-            </div>
 
-            {children}
+                {children}
+            </div>
 
         </section>
     )
