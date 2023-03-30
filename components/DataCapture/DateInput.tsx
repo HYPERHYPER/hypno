f3647d09cd18daef9feeb44b13815c000098f4a3
Sydney Essex <sydney@vibe.video>
2023-03-30 14:19:05 -0400

    fix: mobile date input
    fix: date input
    
    fix: date input
    
    fix: date input again
    
    fix: data input AGAIN

--- components/DataCapture/DateInput.tsx
+++ components/DataCapture/DateInput.tsx
@@ -11,19 +11,21 @@ export const DateInput = forwardRef<HTMLInputElement, DateInputProps>((props, re
     const [isFocused, setIsFocused] = useState<boolean>(false);
 
     return (
-        <div className="relative input">
+        <div className="relative h-12 w-full">
             <input
                 {...props}
                 type='date'
                 ref={ref}
-                className={`input data-capture w-full absolute top-0 left-0 ${props.error && 'error text-red-600'}`}
+                className={`input data-capture w-full flex items-center absolute inset-0 ${props.error && 'error text-red-600'}`}
                 onFocus={() => setIsFocused(true)}
                 onBlur={() => setIsFocused(false)}
+                style={{ boxSizing: 'border-box' }}
             />
             {!isFocused && !props.hasvalue &&
                 <div 
                     onClick={() => setIsFocused(true)}
-                    className={`absolute inset-0 flex items-center w-full data-capture input ${props.error && 'error text-red-600'}`}>{props.error ? `${props.name} is required` : props.name}</div>
+                    style={{ boxSizing: 'border-box' }}
+                    className={`absolute inset-0 flex items-center w-full input data-capture border-none ${props.error && 'error text-red-600'}`}>{props.error ? `${props.name} is required` : props.name}</div>
             }
         </div>
     )
