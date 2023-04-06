a8c7d9479b75f5291e1b810ae469503bb8d7ed2a
Sydney Essex <sydney@vibe.video>
2023-04-06 12:41:54 -0400

    fix: ai remix layout

--- components/Gallery/DetailView.tsx
+++ components/Gallery/DetailView.tsx
@@ -15,7 +15,7 @@ export default function DetailView({ asset, config, imageProps }: any) {
 
     const { output, generateImgToImg, generateTextInpainting, isLoading: isLoadingGeneration } = useStableDiffusion();
     const handleRemix = async (e: any) => {
-        e.preventDefault();
+        // e.preventDefault();
         if (config.aiGeneration) {
             const buffer = await fetch(`/api/file?url=${asset.url}`)
                 .then((res) => res.json())
@@ -74,25 +74,11 @@ export default function DetailView({ asset, config, imageProps }: any) {
                         </div>
                     )}
                 </div>
-                {(isLoadingGeneration || output) && (
-                    <div className='relative bg-white/10 backdrop-blur-[50px] max-h-[75vh] sm:h-[75vh] mt-8'>
-                        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10'>
-                            <Spinner />
-                        </div>
 
-                        <div className='block'>
-                            <img src={String(output)} alt={asset.event_name + asset.id} className='max-h-[75vh] sm:min-w-[512px] sm:h-[75vh]' />
-                            {/* <AutosizeImage
-                                        src={photo.url}
-                                        alt={photo.event_name + photo.id}
-                                    /> */}
-                        </div>
-                    </div>
-                )}
                 <div className='hidden sm:block sm:mt-3'>
                     {asset.mp4_url && <a className='btn btn-primary btn-gallery locked' href={asset.download_url}>download ↓</a>}
                     {(!asset.mp4_url && config?.aiGeneration && config.aiGeneration.enabled) && (
-                        <button className='btn btn-info btn-gallery locked' onClick={handleRemix}>
+                        <label htmlFor="my-modal" className='btn btn-info btn-gallery locked' onClick={handleRemix}>
                             {isLoadingGeneration ?
                                 <ThreeDots
                                     height="10"
@@ -102,7 +88,7 @@ export default function DetailView({ asset, config, imageProps }: any) {
                                     ariaLabel="three-dots-loading"
                                     visible={true}
                                 /> : 'remix ☢︎'}
-                        </button>
+                        </label>
                     )}
                 </div>
             </div>
@@ -110,7 +96,7 @@ export default function DetailView({ asset, config, imageProps }: any) {
             <div className='block sm:hidden'>
                 {asset.mp4_url && <a className='btn btn-primary btn-gallery locked' href={asset.download_url}>download ↓</a>}
                 {(!asset.mp4_url && config?.aiGeneration && config.aiGeneration.enabled) && (
-                    <button className='btn btn-info btn-gallery locked' onClick={handleRemix}>
+                    <label htmlFor="my-modal" className='btn btn-info btn-gallery locked' onClick={handleRemix}>
                         {isLoadingGeneration ?
                             <ThreeDots
                                 height="10"
@@ -120,9 +106,24 @@ export default function DetailView({ asset, config, imageProps }: any) {
                                 ariaLabel="three-dots-loading"
                                 visible={true}
                             /> : 'remix ☢︎'}
-                    </button>
+                    </label>
                 )}
             </div>
+
+            <input type="checkbox" id="my-modal" className="modal-toggle" />
+            <label htmlFor="my-modal" className="modal h-screen mt-0 backdrop-blur-[30px]">
+                <label htmlFor='' className="modal-box bg-transparent shadow-none sm:max-w-5xl">
+                    <div className='relative bg-white/10 backdrop-blur-[50px] aspect-square w-auto max-h-[75vh] sm:h-[75vh] mx-auto'>
+                        <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10'>
+                            <Spinner />
+                        </div>
+
+                        <div className='block'>
+                            <img src={String(output)} alt={asset.event_name + asset.id} className='max-h-[75vh] w-auto sm:min-w-[512px] sm:h-[75vh]' />
+                        </div>
+                    </div>
+                </label>
+            </label>
         </>
     )
 }
