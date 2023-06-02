73ca204792d47cec2168e64db5aa33c8241d78c6
Sydney Essex <sydney@vibe.video>
2023-06-02 13:53:47 -0400

    fix: lowercase footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 export const Footer = () => {
     return (
         <footer className="text-center block lg:fixed lg:bottom-[35px] lg:right-[60px]">
-            <div className="flex flex-row gap-3 text-grey items-baseline justify-center lg:justify-end lg:text-lg lg:gap-8">
-                <span>© Hypno 2023</span>
-                <a href="https://hypno.com/privacy" target='_blank' rel="noreferrer">Terms</a>
+            <div className="lowercase flex flex-row gap-3 text-grey items-baseline justify-center lg:justify-end lg:text-lg lg:gap-8">
+                <span>© hypno 2023</span>
+                <a href="https://hypno.com/privacy" target='_blank' rel="noreferrer">terms</a>
             </div>
         </footer>
     )
