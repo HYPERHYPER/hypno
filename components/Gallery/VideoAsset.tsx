086646b41c442b639c1431734a00473673ebda13
Sydney Essex <sydney@vibe.video>
2023-06-05 14:14:02 -0400

    fix: update sound icon

--- components/Gallery/VideoAsset.tsx
+++ components/Gallery/VideoAsset.tsx
@@ -1,5 +1,5 @@
 import { CSSProperties, useCallback, useRef, useState } from "react";
-import SpeakerOff from '../../public/pop/speaker-off.svg'
+import Sound from '../../assets/icons/sound.svg'
 
 export default function VideoAsset({ src, poster, style }: { src?: string; poster?: string, style?: CSSProperties}) {
     const vidRef = useRef<HTMLVideoElement>(null);
@@ -16,7 +16,7 @@ export default function VideoAsset({ src, poster, style }: { src?: string; poste
         <div className='block relative' style={style}>
             {muted && (
                 <div className="absolute top-[10px] left-[10px] z-10">
-                    <button onClick={unmuteVideo} className="cursor-pointer w-[35px] h-[35px] sm:w-[50px] sm:h-[50px] btn-circle bg-black flex items-center justify-center"><SpeakerOff /></button>
+                    <button onClick={unmuteVideo} className="cursor-pointer flex items-center justify-center"><span className="scale-50 -translate-x-1/4 -translate-y-1/4"><Sound /></span></button>
                 </div>
             )}
             <video ref={vidRef} style={style} className='w-full h-auto' src={src} autoPlay loop playsInline muted poster={poster} />
