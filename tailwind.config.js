a85fc5c95a0edd54ad2e4e540e81b51859795fb2
Sydney Essex <sydney@vibe.video>
2023-06-06 17:05:59 -0400

    fix: date picker packages

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
     fontFamily: {
