2f0cd1e11d658c1e01c8308a80170d59d4d1648a
Sydney Essex <sydney@vibe.video>
2023-06-06 15:24:41 -0400

    fix: add download btn for all content

--- components/Gallery/DetailView.tsx
+++ components/Gallery/DetailView.tsx
@@ -7,7 +7,9 @@ import clsx from "clsx";
 import VideoAsset from "./VideoAsset";
 
 export default function DetailView({ asset, config, imageProps }: any) {
-    const footer = Boolean(config.aiGeneration?.enabled || asset.mp4_url);
+    // const footer = Boolean(config.aiGeneration?.enabled || asset.mp4_url);
+    const footer = true; // always show download btn
+    const [assetHeight, setAssetHeight] = useState<number>(0);
 
     const { output, generateImgToImg, generateTextInpainting, isLoading: isLoadingGeneration } = useStableDiffusion();
     const handleRemix = async (e: any) => {
@@ -54,39 +56,27 @@ export default function DetailView({ asset, config, imageProps }: any) {
                     )}
                 </div>
 
-                <div className='hidden sm:block sm:mt-3'>
-                    {asset.mp4_url && <a className='btn btn-primary btn-gallery locked' href={asset.download_url}>download ↓</a>}
-                    {(!asset.mp4_url && config?.aiGeneration && config.aiGeneration.enabled) && (
-                        <label htmlFor="my-modal" className='btn btn-info btn-gallery locked' onClick={handleRemix}>
-                            {isLoadingGeneration ?
-                                <ThreeDots
-                                    height="10"
-                                    width="30"
-                                    radius="4"
-                                    color="#FFFFFF"
-                                    ariaLabel="three-dots-loading"
-                                    visible={true}
-                                /> : 'remix ☢︎'}
-                        </label>
-                    )}
-                </div>
+                        <div className='hidden sm:block sm:mt-3 sm:text-center'>
+                            <a className='btn btn-primary btn-gallery locked sm:max-w-sm' href={asset.download_url}>download ↓</a>
+                            {(config?.aiGeneration && config.aiGeneration.enabled) && (
+                                <label htmlFor="my-modal" className='btn btn-info btn-gallery locked' onClick={handleRemix}>
+                                    {isLoadingGeneration ?
+                                        <ThreeDots
+                                            height="10"
+                                            width="30"
+                                            radius="4"
+                                            color="#FFFFFF"
+                                            ariaLabel="three-dots-loading"
+                                            visible={true}
+                                        /> : 'remix ☢︎'}
+                                </label>
+                            )}
+                        </div>
+                    </div>
             </div>
 
             <div className='block sm:hidden'>
-                {asset.mp4_url && <a className='btn btn-primary btn-gallery locked' href={asset.download_url}>download ↓</a>}
-                {(!asset.mp4_url && config?.aiGeneration && config.aiGeneration.enabled) && (
-                    <label htmlFor="my-modal" className='btn btn-info btn-gallery locked' onClick={handleRemix}>
-                        {isLoadingGeneration ?
-                            <ThreeDots
-                                height="10"
-                                width="30"
-                                radius="4"
-                                color="#FFFFFF"
-                                ariaLabel="three-dots-loading"
-                                visible={true}
-                            /> : 'remix ☢︎'}
-                    </label>
-                )}
+                <a className='btn btn-primary btn-gallery locked' href={asset.download_url}>download ↓</a>
             </div>
 
             <input type="checkbox" id="my-modal" className="modal-toggle" />
