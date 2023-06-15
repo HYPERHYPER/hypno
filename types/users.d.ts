19842b45a5a566b7283332c585b446cf2277246c
Sydney Essex <sydney@vibe.video>
2023-06-15 15:18:51 -0400

    fix: build err missing user type

--- types/users.d.ts
+++ types/users.d.ts
@@ -1,9 +1,9 @@
-enum UserRole {
-    Owner = 13,
-    Admin = 2,
-    Member = 8,
-    FreeAgent = 222,
-    Hypno = 0,
+export type UserRole = {
+    Owner: 13,
+    Admin: 2,
+    Member: 3,
+    FreeAgent: 222,
+    Hypno: 0,
 }
 
 enum UserType {
@@ -12,4 +12,20 @@ enum UserType {
     Studio,
     Partner, // Agency/Verified
     Enterprise,
+}
+
+export interface NewUser {
+    first_name: string;
+    last_name: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+// If hashed will be a string
+// If sending invite, will be decrypted version
+export interface UserInvite {
+    inviter_user_id: string | number;
+    organization_id: string | number;
+    role: string | number;
 }
