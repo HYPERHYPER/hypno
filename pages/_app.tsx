6ab4da184bad9f429c2950cddc4948a96c7c3e77
Sydney Essex <sydney@vibe.video>
2023-06-27 12:19:15 -0400

    feat: google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,20 @@ import type { AppProps } from 'next/app';
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { Analytics } from '@vercel/analytics/react';
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+const TRACKING_ID = "AW-860626681"; // OUR_TRACKING_ID
+ReactGA.initialize(TRACKING_ID);
 
 const stripePromise = loadStripe(
   `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}` as string
 );
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+  
   return (
       <Elements stripe={stripePromise}>
         <Component {...pageProps} />
