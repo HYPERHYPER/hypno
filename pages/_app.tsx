6c8b80cff5fef04f5c381045f4b3d32430c20562
Sydney Essex <sydney@vibe.video>
2023-07-10 07:51:53 -0400

    feat: add gtag script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,22 +5,31 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Analytics } from '@vercel/analytics/react';
 import ReactGA from 'react-ga';
 import { useEffect } from 'react';
-const TRACKING_ID = "AW-860626681"; // OUR_TRACKING_ID
-ReactGA.initialize(TRACKING_ID);
+import Script from 'next/script';
+const TRACKING_ID = "G-93Y5N61TVT"; // OUR_TRACKING_ID
+// ReactGA.initialize(TRACKING_ID);
 
 const stripePromise = loadStripe(
   `${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}` as string
 );
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-  
+  // useEffect(() => {
+  //   ReactGA.pageview(window.location.pathname + window.location.search);
+  // }, []);
+
   return (
-      <Elements stripe={stripePromise}>
-        <Component {...pageProps} />
-        <Analytics />
-      </Elements>
+    <Elements stripe={stripePromise}>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-93Y5N61TVT" />
+      <Script id='google-tag' onLoad={() => console.log('Gtag script loaded')}>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-93Y5N61TVT');`}
+      </Script>
+      <Component {...pageProps} />
+      <Analytics />
+    </Elements>
   );
 }
