a76549eb9edb02f410621c06d582190875c8619b
Sydney Essex <sydney@vibe.video>
2023-08-08 12:52:39 -0400

    feat: agency -> studio

--- components/Plans/PlansModal.tsx
+++ components/Plans/PlansModal.tsx
@@ -6,27 +6,27 @@ import _ from 'lodash';
 import { useRouter } from 'next/router';
 import Checkmark from 'public/pop/checkmark.svg';
 import Minus from 'public/pop/minus.svg';
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import useSWR from 'swr';
 
 interface FeatureAccess {
     [key: string]: Array<string>;
 }
 const FEATURE_ACCESS: FeatureAccess = {
-    "camera": ['free', 'creator', 'agency', 'brand'],
-    "filters": ['free', 'creator', 'agency', 'brand'],
-    "graphics": ['creator', 'agency', 'brand'],
-    "effects": ['creator', 'agency', 'brand'],
-    "canon": ['agency', 'brand'],
-    "gallery": ['free', 'creator', 'agency', 'brand'],
-    "branding": ['creator', 'agency', 'brand'],
+    "camera": ['free', 'creator', 'studio', 'brand'],
+    "filters": ['free', 'creator', 'studio', 'brand'],
+    "graphics": ['creator', 'studio', 'brand'],
+    "effects": ['creator', 'studio', 'brand'],
+    "canon": ['studio', 'brand'],
+    "gallery": ['free', 'creator', 'studio', 'brand'],
+    "branding": ['creator', 'studio', 'brand'],
     "data capture": ['brand'],
     "custom legal": ['brand'],
     "your domain": ['brand'],
     "api access": ['brand'],
-    "live support": ['agency', 'brand'],
-    "studio services": ['agency', 'brand'],
-    "hypno community": ['agency', 'brand']
+    "live support": ['studio', 'brand'],
+    "studio services": ['studio', 'brand'],
+    "hypno community": ['studio', 'brand']
 }
 
 const FEATURES = [
@@ -76,7 +76,7 @@ const PLAN_TYPES: PlanType = {
         additionalPrice: 9,
         uploads: 'unlimited'
     },
-    agency: {
+    studio: {
         tagline: 'canon capture',
         annualPrice: 999,
         monthlyPrice: 1250,
@@ -98,8 +98,8 @@ const PLAN_TYPES: PlanType = {
 const CREATOR_PLAN = 'price_1NT8F8A3gTSa41Ce8riWJDJw'
 const CREATOR_PLAN_DISCOUNTED = 'price_1NQxqWA3gTSa41Ce2ofRCgn4'
 
-const AGENCY_PLAN = 'price_1NT8G6A3gTSa41CeZ2XYleYk'
-const AGENCY_PLAN_DISCOUNTED = 'price_1NQxrDA3gTSa41CeORPpKPGl'
+const STUDIO_PLAN = 'price_1NT8G6A3gTSa41CeZ2XYleYk'
+const STUDIO_PLAN_DISCOUNTED = 'price_1NQxrDA3gTSa41CeORPpKPGl'
 
 const BRAND_PLAN = 'price_1NT8HLA3gTSa41CeELa2qyxq'
 const BRAND_PLAN_DISCOUNTED = 'price_1NQxrWA3gTSa41Ce8cmJ1j77'
@@ -107,7 +107,7 @@ const BRAND_PLAN_DISCOUNTED = 'price_1NQxrWA3gTSa41Ce8cmJ1j77'
 const getProductId = (plan_type: string, subscription: 'annual' | 'monthly') => {
     switch (plan_type) {
         case 'creator': return subscription == 'monthly' ? CREATOR_PLAN : CREATOR_PLAN_DISCOUNTED;
-        case 'agency': return subscription == 'monthly' ? AGENCY_PLAN : AGENCY_PLAN_DISCOUNTED;
+        case 'studio': return subscription == 'monthly' ? STUDIO_PLAN : STUDIO_PLAN_DISCOUNTED;
         case 'brand': return subscription == 'monthly' ? BRAND_PLAN : BRAND_PLAN_DISCOUNTED;
         default: '';
     }
@@ -160,7 +160,6 @@ export default function PaymentPlansModal() {
         ([url, token]) => axiosGetWithToken(url, token))
 
     const orgTier = orgData?.organization.metadata.hypno_pro.current_tier;
-    console.log(orgData?.organization)
 
     const [billingFrequency, setBillingFrequency] = useState<'annual' | 'monthly'>('monthly');
 
@@ -230,8 +229,9 @@ export default function PaymentPlansModal() {
 
     }, [orgTier, billingFrequency])
 
-
-
+    // useEffect(() => {
+    //     console.log('owner', window?.isPaymentPlanOwner);
+    // }, []);
 
     return (
         <dialog id="payment_plans_modal" className="modal bg-[#333333]/50 backdrop-blur-[20px] cursor-pointer">
@@ -262,7 +262,7 @@ export default function PaymentPlansModal() {
                         </div>
 
                         <div className="mt-6 grid grid-cols-2 lg:grid-cols-4 gap-3 sm:gap-6">
-                            {_.map(['free', 'creator', 'agency', 'brand'], (type, i) => (
+                            {_.map(['free', 'creator', 'studio', 'brand'], (type, i) => (
                                 <PlanCard key={i} type={type} current={orgTier} onPlanSelect={handlePlanSelect} billingFrequency={billingFrequency} />
                             ))}
                         </div>
