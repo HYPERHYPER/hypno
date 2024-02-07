c8c618b19053ce258e5f8c29032d11fa5104babc
Sydney Essex <sydney@vibe.video>
2024-02-07 16:56:30 -0500

    fix: simplify data capture date input

--- components/DataCapture/DateInput.tsx
+++ components/DataCapture/DateInput.tsx
@@ -1,14 +1,8 @@
 import { forwardRef, useState, InputHTMLAttributes } from "react";
 import _ from 'lodash';
-import Datepicker from "react-tailwindcss-datepicker";
-import { DateValueType } from "react-tailwindcss-datepicker/dist/types";
-import DatePicker from 'react-date-picker';
 import 'react-date-picker/dist/DatePicker.css';
 import 'react-calendar/dist/Calendar.css';
 
-
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 interface DateInputProps extends InputHTMLAttributes<HTMLInputElement> {
     placeholder?: string;
@@ -16,57 +10,26 @@ interface DateInputProps extends InputHTMLAttributes<HTMLInputElement> {
     updateValue?: (value?: any) => void;
 }
 
-// const isValidDate = (value: string) => !_.isEmpty(value) && value !== 'Invalid Date'
-
 export const DateInput = forwardRef<HTMLInputElement, DateInputProps>(({ ...rest }, ref) => {
     const [focused, setFocused] = useState<boolean>(false);
-    const [value, setValue] = useState<Value>(null)
-    const handleValueChange = (newValue: Value) => {
-        setValue(newValue);
-        rest.updateValue && rest.updateValue(newValue);
-        if (_.isNil(newValue)) setFocused(false);
+    const [value, setValue] = useState<string>('')
+    const handleValueChange = (e: any) => {
+        const value = e.target.value
+        setValue(value);
+        rest.updateValue && rest.updateValue(value);
+        if (_.isNil(value)) setFocused(false);
     }
-
-    {/* <input
-                {...rest}
-                placeholder={rest.name}
-                type='date'
-                ref={ref}
-                className={`input data-capture w-full absolute inset-0 ${rest.error && 'error text-red-600'}`}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-                style={{ boxSizing: 'border-box' }}
-            />
-            {!isFocused && !isValidDate(String(value)) &&
-                <div 
-                    onClick={() => setIsFocused(true)}
-                    style={{ boxSizing: 'border-box' }}
-                    className={`absolute inset-0 flex items-center w-full input data-capture border-none ${rest.error && 'error text-red-600'}`}>{rest.error ? `${rest.placeholder} is required` : rest.placeholder}</div>
-            } */}
 
     return (
         <div className="relative">
-            <DatePicker
-                calendarClassName='bg-white'
+            <input 
+                type="date" 
                 className={`absolute inset-0 input data-capture w-full ${rest.error && 'error text-red-600'}`}
                 onChange={handleValueChange}
                 value={value}
-                maxDate={new Date()}
-            />
+                />
             {!focused && <input className="absolute inset-0 input data-capture cursor-pointer" placeholder="birthday" onClick={() => setFocused(true)} />}
         </div>
-
-        // <Datepicker
-        //     {...rest}
-        //     inputId={rest.id}
-        //     value={value}
-        //     asSingle={true}
-        //     placeholder={rest.placeholder}
-        //     onChange={handleValueChange}
-        //     useRange={false}
-        //     maxDate={new Date()}
-        //     inputClassName={`input data-capture w-full ${rest.error && 'error text-red-600'}`}
-        // />
     )
 })
 
