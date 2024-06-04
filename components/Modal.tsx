41701ec5273798789a2ad1d1b1eab95a8f8718f2
bryanmoon1991 <bryanmoon1991@gmail.com>
2024-06-03 17:50:00 -0700

    adds guest removal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,70 +1,166 @@
 import { ReactNode } from "react";
 import { SaveStatus } from "./Form/AutosaveStatusText";
-import Spinner from "./Spinner";
+
+interface OrgUser {
+  id: number | null;
+  username: string | null;
+  organization_id: number | null;
+  email: string | null;
+  first_name: string | null;
+  last_name: string | null;
+  avatar: string | null;
+  created_at: string | null;
+  roles: Role[] | string | null;
+}
+
+interface Role {
+  id: number;
+  user_id: number;
+  event_id: number;
+  organization_id: number | null;
+  created_at: string;
+  updated_at: string;
+  authorizer_id: number;
+  kind: string;
+  status: string;
+}
 
 interface TriggerModalProps {
-    id?: string;
-    children?: ReactNode;
+  id?: string;
+  children?: ReactNode;
+  onClick?: () => void;
 }
 
 interface ModalProps {
-    id?: string;
-    children?: ReactNode;
-    title?: string;
-    onDone?: () => void;
-    menu?: ReactNode;
-    actionBtn?: {
-        status?: SaveStatus;
-        text?: string;
-        onClick?: () => void;
-        hidden?: boolean;
-    };
+  id?: string;
+  children?: ReactNode;
+  title?: string;
+  onDone?: () => void;
+  menu?: ReactNode;
+  actionBtn?: {
+    status?: SaveStatus;
+    text?: string;
+    onClick?: () => void;
+    hidden?: boolean;
+  };
+  user?: OrgUser;
 }
 
-const TriggerModal = ({ id, children }: TriggerModalProps) => {
-    return (
-        <label htmlFor={id} className="cursor-pointer">{children}</label>
-    )
-}
+const TriggerModal = ({ id, children, onClick }: TriggerModalProps) => {
+  return (
+    <label htmlFor={id} className="cursor-pointer" onClick={onClick}>
+      {children}
+    </label>
+  );
+};
 
-const btnClassName = "tracking-tight btn btn-primary rounded-[20px] btn-block h-[60px] text-2xl cursor-pointer";
+const btnClassName =
+  "tracking-tight btn btn-primary rounded-[20px] btn-block h-[60px] text-2xl cursor-pointer";
 
-export default function Modal({ title, id, children, onDone, menu, actionBtn }: ModalProps) {
-    return (
-        <>
-            <input type="checkbox" id={id} className="modal-toggle" />
-            <label htmlFor={id} className="modal bg-[#333333]/50 backdrop-blur-[20px] cursor-pointer">
-                <label htmlFor="" className="modal-box max-w-3xl px-[40px] py-[35px] relative bg-black rounded-[60px] tracking-tight overflow-clip">
-                    <div className="flex justify-between">
-                        <div className="space-y-4">
-                            <h1 className="text-white">{title}</h1>
-                            <div className="flex flex-row gap-4">
-                                {/* <h2 className="text-primary"><label htmlFor={id} className="cursor-pointer">cancel</label></h2> */}
-                                {menu}
-                            </div>
-                        </div>
-                        <label htmlFor={id} className="h-[30px] sm:h-[60px] w-[30px] sm:w-[60px] flex items-center  cursor-pointer">
-                            <div className="bg-white/40 w-[30px] sm:w-[60px] h-1 rounded-sm" />
-                        </label>
-                    </div>
-
-                    <div className={`pr-2 mt-5 sm:mt-10 mb-9 max-h-[50vh] overflow-y-scroll ${actionBtn && actionBtn.hidden ? 'mb-0' : 'mb-9'}`}>
-                        {children}
-                    </div>
-
-                    {actionBtn ? (actionBtn.hidden ? null : (
-                        <button onClick={actionBtn.onClick} className={btnClassName} disabled={actionBtn.status == 'success' || actionBtn.status == 'saving' }>
-                            {(actionBtn.status == 'ready' || actionBtn.status == 'error') && actionBtn.text}
-                            {actionBtn.status == 'saving' && <span className="loading loading-dots"></span>}
-                            {actionBtn.status == 'success' && 'success!'}
-                        </button>
-                    )) : (
-                        <label htmlFor={id} onClick={onDone ? onDone : undefined} className={btnClassName}>done</label>
-                    )}
-                </label>
+export default function Modal({
+  title,
+  id,
+  children,
+  onDone,
+  menu,
+  actionBtn,
+  user,
+}: ModalProps) {
+  return (
+    <>
+      <input type="checkbox" id={id} className="modal-toggle" />
+      <label
+        htmlFor={id}
+        className="modal bg-[#333333]/50 backdrop-blur-[20px] cursor-pointer"
+      >
+        <label
+          htmlFor=""
+          className="modal-box max-w-3xl px-[40px] py-[35px] relative bg-black rounded-[60px] tracking-tight overflow-clip"
+        >
+          <div className="flex justify-between">
+            <div className="space-y-4">
+              <h1 className="text-white">{title}</h1>
+              <div className="flex flex-row gap-4">{menu}</div>
+            </div>
+            <label
+              htmlFor={id}
+              className="h-[30px] sm:h-[60px] w-[30px] sm:w-[60px] flex items-center  cursor-pointer"
+            >
+              <div className="bg-white/40 w-[30px] sm:w-[60px] h-1 rounded-sm" />
             </label>
-        </>
-    )
+          </div>
+
+          <div
+            className={`pr-2 mt-5 sm:mt-10 mb-9 max-h-[50vh] overflow-y-scroll ${
+              actionBtn && actionBtn.hidden ? "mb-0" : "mb-9"
+            }`}
+          >
+            {children}
+            {/* {user && (
+              <div className="user-details">
+                <p>Username: {user.username}</p>
+                <p>Email: {user.email}</p>
+                <ul>
+                  {Array.isArray(user.roles) &&
+                    user.roles.map((role, i) => (
+                      <li key={i}>Event: {role.event_id}</li>
+                    ))}
+                </ul>
+              </div>
+            )} */}
+            {user && (
+              <div className="user-details p-4 bg-black-800 rounded-lg text-white">
+                <p className="text-lg font-semibold">
+                  Username: <span className="font-normal">{user.username}</span>
+                </p>
+                <p className="text-lg font-semibold">
+                  Email: <span className="font-normal">{user.email}</span>
+                </p>
+                <ul className="mt-2 space-y-2">
+                  {Array.isArray(user.roles) &&
+                    user.roles.map((role, i) => (
+                      <li
+                        key={i}
+                        className="text-sm bg-black-700 rounded-md p-2"
+                      >
+                        Event: {role.event_id}
+                      </li>
+                    ))}
+                </ul>
+              </div>
+            )}
+          </div>
+
+          {actionBtn ? (
+            actionBtn.hidden ? null : (
+              <button
+                onClick={actionBtn.onClick}
+                className={btnClassName}
+                disabled={
+                  actionBtn.status == "success" || actionBtn.status == "saving"
+                }
+              >
+                {(actionBtn.status == "ready" || actionBtn.status == "error") &&
+                  actionBtn.text}
+                {actionBtn.status == "saving" && (
+                  <span className="loading loading-dots"></span>
+                )}
+                {actionBtn.status == "success" && "success!"}
+              </button>
+            )
+          ) : (
+            <label
+              htmlFor={id}
+              onClick={onDone ? onDone : undefined}
+              className={btnClassName}
+            >
+              done
+            </label>
+          )}
+        </label>
+      </label>
+    </>
+  );
 }
 
 Modal.Trigger = TriggerModal;
--- pages/org/index.tsx
+++ pages/org/index.tsx
@@ -1,165 +1,205 @@
-import Head from 'next/head'
-import _, { debounce } from 'lodash';
-import useUserStore from '@/store/userStore';
-import withAuth from '@/components/hoc/withAuth';
-import GlobalLayout from '@/components/GlobalLayout';
-import Link from 'next/link';
-import Modal from '@/components/Modal';
-import FormControl from '@/components/Form/FormControl';
-import { GetServerSideProps } from 'next';
-import axios from 'axios';
-import nookies from 'nookies';
-import { useCallback, useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { AutosaveStatusText, SaveStatus } from '@/components/Form/AutosaveStatusText';
-import { axiosGetWithToken } from '@/lib/fetchWithToken';
-import useSWR from 'swr';
-import { getOrganizationPrivileges } from '@/helpers/user-privilege';
+import Head from "next/head";
+import _, { debounce } from "lodash";
+import useUserStore from "@/store/userStore";
+import withAuth from "@/components/hoc/withAuth";
+import GlobalLayout from "@/components/GlobalLayout";
+import Link from "next/link";
+import Modal from "@/components/Modal";
+import FormControl from "@/components/Form/FormControl";
+import { GetServerSideProps } from "next";
+import axios from "axios";
+import nookies from "nookies";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  AutosaveStatusText,
+  SaveStatus,
+} from "@/components/Form/AutosaveStatusText";
+import { axiosGetWithToken } from "@/lib/fetchWithToken";
+import useSWR from "swr";
+import { getOrganizationPrivileges } from "@/helpers/user-privilege";
 
 interface ResponseData {
-    user_count: number;
+  user_count: number;
 }
 
-export default withAuth(OrganizationSettingsPage, 'protected');
+export default withAuth(OrganizationSettingsPage, "protected");
 function OrganizationSettingsPage(props: ResponseData) {
-    const user = useUserStore.useUser();
-    const updateUser = useUserStore.useUpdateUser();
-    const token = useUserStore.useToken();
+  const user = useUserStore.useUser();
+  const updateUser = useUserStore.useUpdateUser();
+  const token = useUserStore.useToken();
 
-    const orgUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/hypno/v1/organizations/${user.organization.id}`;
-    const { data: orgData, isValidating: isValidatingOrgData, error: orgError } = useSWR([orgUrl, token.access_token],
-        ([url, token]) => axiosGetWithToken(url, token))
+  const orgUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/hypno/v1/organizations/${user.organization.id}`;
+  const {
+    data: orgData,
+    isValidating: isValidatingOrgData,
+    error: orgError,
+  } = useSWR([orgUrl, token.access_token], ([url, token]) =>
+    axiosGetWithToken(url, token),
+  );
 
-    const organization = orgData?.organization || user.organization;
+  const organization = orgData?.organization || user.organization;
+  console.log(organization);
 
-    const userUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/hypno/v1/users?per_page=1`;
-    const { data: userData, isValidating: isValidatingUserData, error: userError } = useSWR([userUrl, token.access_token],
-        ([url, token]) => axiosGetWithToken(url, token))
-    //@ts-ignore
-    const userCount = userData?.meta?.total_count || 0;
+  const userUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/hypno/v1/users?per_page=1`;
+  const {
+    data: userData,
+    isValidating: isValidatingUserData,
+    error: userError,
+  } = useSWR([userUrl, token.access_token], ([url, token]) =>
+    axiosGetWithToken(url, token),
+  );
+  //@ts-ignore
+  const userCount = userData?.meta?.total_count || 0;
 
-    const [saveStatus, setSaveStatus] = useState<SaveStatus>('ready');
-    const {
-        register,
-        handleSubmit,
-        formState: { isDirty, errors },
-        reset
-    } = useForm({
-        defaultValues: {
-            name: organization.name || '',
-        }
-    });
+  const [saveStatus, setSaveStatus] = useState<SaveStatus>("ready");
+  const {
+    register,
+    handleSubmit,
+    formState: { isDirty, errors },
+    reset,
+  } = useForm({
+    defaultValues: {
+      name: organization.name || "",
+    },
+  });
 
-    const updateOrganizationName = async (data: any) => {
-        if (!_.isEmpty(errors)) {
-            console.log("submitForm errors", { errors });
-            setSaveStatus('error');
-            return;
-        }
-
-        /* Update user payload */
-        let payload = {
-            organization: {
-                ...data
-            }
-        }
-
-        const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/hypno/v1/organizations/${organization.id}`;
-        await axios.put(url, payload, {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + token.access_token,
-            },
-        }).then((res) => {
-            setSaveStatus('success')
-            setTimeout(() => {
-                setSaveStatus('ready')
-            }, 3000)
-            updateUser({
-                organization: {
-                    ...data
-                }
-            });
-            reset(data);
-        }).catch((e) => {
-            console.log(e);
-            setSaveStatus('error')
-        })
+  const updateOrganizationName = async (data: any) => {
+    if (!_.isEmpty(errors)) {
+      console.log("submitForm errors", { errors });
+      setSaveStatus("error");
+      return;
     }
 
-    const debouncedSave = useCallback(
-        debounce(() => {
-            handleSubmit(updateOrganizationName)();
-            return;
-        }, 1000),
-        []
-    );
+    /* Update user payload */
+    let payload = {
+      organization: {
+        ...data,
+      },
+    };
 
-    useEffect(() => {
-        if (isDirty) {
-            setSaveStatus('saving');
-            debouncedSave();
-        }
-    }, [isDirty]);
+    const url = `${process.env.NEXT_PUBLIC_API_BASE_URL}/hypno/v1/organizations/${organization.id}`;
+    await axios
+      .put(url, payload, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + token.access_token,
+        },
+      })
+      .then((res) => {
+        setSaveStatus("success");
+        setTimeout(() => {
+          setSaveStatus("ready");
+        }, 3000);
+        updateUser({
+          organization: {
+            ...data,
+          },
+        });
+        reset(data);
+      })
+      .catch((e) => {
+        console.log(e);
+        setSaveStatus("error");
+      });
+  };
 
-    const userOrgPrivileges = orgData ? getOrganizationPrivileges(orgData.organization.user_privileges) : null;
+  const debouncedSave = useCallback(
+    debounce(() => {
+      handleSubmit(updateOrganizationName)();
+      return;
+    }, 1000),
+    [],
+  );
 
-    return (
-        <>
-            <Head>
-                <title>organization settings | hypno™</title>
-                <meta name="description" content="shoot + share professionally branded content — in seconds — from live events, shoots, stores and more. trusted by NBA, Dolby, TikTok, Apple, Bacardi, New Balance and other world-class brands." />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <GlobalLayout>
-                <GlobalLayout.Header
-                    title='organization'
-                    returnLink={{ slug: '/settings', name: 'settings' }}
-                >
-                    <h2 className='text-white/40'>since 2023</h2>
-                </GlobalLayout.Header>
-                <GlobalLayout.Content>
-                    <div className='list pro'>
-                        {userOrgPrivileges?.canEditOrganization ? (
-                            <Modal.Trigger id='org-name-modal'><Item name='org name' value={organization.name} /></Modal.Trigger>
-                        ) : (
-                            <Item name='org name' value={organization.name} />
-                        )}
-                        {/* <Item name='org events' value={'#'} /> */}
-                        {userOrgPrivileges?.canViewUsers && <Item name='org users' value={String(userCount)} href='/org/users' />}
-                        {/* <Item name='next payment' value={user.organization_id} /> */}
-                        {/* <Item name='payment method' value={'update'} /> */}
-                        {/* <Item name='subscription' value={'enterprise'} /> */}
-                    </div>
-                </GlobalLayout.Content>
+  useEffect(() => {
+    if (isDirty) {
+      setSaveStatus("saving");
+      debouncedSave();
+    }
+  }, [isDirty]);
 
-                <Modal
-                    id='org-name-modal'
-                    title='edit org name'
-                    menu={AutosaveStatusText(saveStatus)}
-                >
-                    <div className='list pro'>
-                        <FormControl label='name'>
-                            <input
-                                {...register('name', { required: true })}
-                                className='flex-1 input pro lowercase'
-                            />
-                        </FormControl>
-                    </div>
-                </Modal>
-            </GlobalLayout>
-        </>
-    )
+  const userOrgPrivileges = orgData
+    ? getOrganizationPrivileges(orgData.organization.user_privileges)
+    : null;
+
+  return (
+    <>
+      <Head>
+        <title>organization settings | hypno™</title>
+        <meta
+          name="description"
+          content="shoot + share professionally branded content — in seconds — from live events, shoots, stores and more. trusted by NBA, Dolby, TikTok, Apple, Bacardi, New Balance and other world-class brands."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalLayout>
+        <GlobalLayout.Header
+          title="organization"
+          returnLink={{ slug: "/settings", name: "settings" }}
+        >
+          <h2 className="text-white/40">
+            {organization.metadata?.hypno_pro?.current_tier} plan
+          </h2>
+        </GlobalLayout.Header>
+        <GlobalLayout.Content>
+          <div className="list pro">
+            {userOrgPrivileges?.canEditOrganization ? (
+              <Modal.Trigger id="org-name-modal">
+                <Item name="org name" value={organization.name} />
+              </Modal.Trigger>
+            ) : (
+              <Item name="org name" value={organization.name} />
+            )}
+            {/* <Item name='org events' value={'#'} /> */}
+            {userOrgPrivileges?.canViewUsers && (
+              <Item
+                name="org users"
+                value={String(userCount)}
+                href="/org/users"
+              />
+            )}
+            {/* <Item name='next payment' value={user.organization_id} /> */}
+            {/* <Item name='payment method' value={'update'} /> */}
+            {/* <Item name='subscription' value={'enterprise'} /> */}
+          </div>
+        </GlobalLayout.Content>
+
+        <Modal
+          id="org-name-modal"
+          title="edit org name"
+          menu={AutosaveStatusText(saveStatus)}
+        >
+          <div className="list pro">
+            <FormControl label="name">
+              <input
+                {...register("name", { required: true })}
+                className="flex-1 input pro lowercase"
+              />
+            </FormControl>
+          </div>
+        </Modal>
+      </GlobalLayout>
+    </>
+  );
 }
 
-const Item = ({ name, value, href }: { name: string, value: string, href?: string, }) => {
-    return (
-        <div className='item'>
-            <span className='text-white/40'>{name}</span>
-            <span className='text-primary lowercase'>
-                {href ? <Link href={href}>{value} →</Link> : value}
-            </span>
-        </div>
-    )
-}
+const Item = ({
+  name,
+  value,
+  href,
+}: {
+  name: string;
+  value: string;
+  href?: string;
+}) => {
+  return (
+    <div className="item">
+      <span className="text-white/40">{name}</span>
+      <span className="text-primary lowercase">
+        {href ? <Link href={href}>{value} →</Link> : value}
+      </span>
+    </div>
+  );
+};
