45a8c6e38c7004908a44f1200f22e214e34f44b5
Sydney Essex <sydney@vibe.video>
2024-08-30 16:02:13 -0400

    mobile font to medium + increase rows

--- components/Form/FormControl.tsx
+++ components/Form/FormControl.tsx
@@ -6,7 +6,9 @@ import Question from 'public/pop/question-filled.svg'
 
 export default function FormControl({ label, altLabel, children, dir = 'row', nested, disabled, featureGated }: { label?: string; altLabel?: string; children: ReactNode, dir?: 'row' | 'col', nested?: boolean, disabled?: boolean, featureGated?: 'creator' | 'brand' }) {
     return (
-        <div className={clsx(dir == 'col' ? 'flex-col' : 'flex-row justify-between items-center', "w-full flex flex-wrap gap-x-4 py-2 sm:py-3 border-b-2 border-white/20 first:border-b-2 first:border-white/20 sm:min-h-[60px]")}>
+        <div className={clsx(
+            dir == 'col' ? 'flex-col' : 'flex-row justify-between items-center', 
+            "w-full flex flex-wrap gap-x-4 py-3 sm:py-3 border-b-2 border-white/20 first:border-b-2 first:border-white/20 sm:min-h-[60px] font-medium sm:font-normal")}>
             <label className={clsx("flex flex-row flex-wrap gap-2 items-center transition-colors", disabled ? 'text-white/20 border-white/20' : 'text-white/40 border-white/40')}>
                 <span className="sm:text-3xl flex items-center max-h-2">
                     {nested && <Nested />}{label} {altLabel && <span className="hidden sm:block ml-1 tooltip tooltip-primary max-w-[100px]" data-tip={altLabel}><Question /></span>}
